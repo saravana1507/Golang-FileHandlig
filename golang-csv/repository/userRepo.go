0370883c02b997a3a10c79bb1c0bd14be9c8094a
saravana1507 <130580529+saravana1507@users.noreply.github.com>
2024-05-15 11:18:06 +0530

    Delete golang-csv directory

--- golang-csv/repository/userRepo.go
+++
@@ -1,8 +0,0 @@
-package repository
-
-import "golang-csv/modal"
-
-type UserRepo interface {
-	GetAll(value modal.Responder) (interface{}, error)
-	SetAll(user modal.Responder) error
-}
--- golang-csv/service/userService.go
+++
@@ -1,8 +0,0 @@
-package service
-
-import "golang-csv/modal"
-
-type Userservice interface {
-	Getvalue(key string, value modal.Responder) error
-	Setvalue(key string, value modal.Responder) error
-}
